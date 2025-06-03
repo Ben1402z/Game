118305b2ed1810bbbaa03e68c47d2207b0726ae0
Ben1402z <153769730+Ben1402z@users.noreply.github.com>
2025-06-03 12:10:20 +0200

    Add files via upload
    .

---
+++ Feld.java
@@ -0,0 +1,20 @@
+
+/**
+ * Klasse Feld.
+ *  
+ * @author 
+ */
+public class Feld {
+    /*---------------Attribute-----*/
+    private int x;
+
+    /*---------------Konstruktor---*/
+    public Feld() {
+        System.out.print("Hallo.");
+    }
+
+    /*---------------Methoden------*/    
+
+    
+    
+}
